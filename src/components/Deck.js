71e9183a557238fc8bc16331674ade4d9c51a0be
Colter Ulrich <cju5025@gmail.com>
2021-01-28 11:03:32 -0700

    Adds random pull

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -2,9 +2,11 @@ import { Component } from 'react';
 
 export default class Deck extends Component {
 
+
+
     render () {
         return (
-            <div id="deck-container">
+            <div id="deck-container" onClick={this.props.pullCard}>
                 <img src='https://i.ibb.co/JkzghW9/back.jpg' alt='tarot deck' title='tarot deck'></img>
             </div>
         )
