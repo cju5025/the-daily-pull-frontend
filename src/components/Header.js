1e44373ca42df89a29f1a067384c82987fecff9f
Colter Ulrich <cju5025@gmail.com>
2021-01-29 11:12:57 -0700

    adds logo and styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 export default function Header () {
     return (
         <header id="header">
+            <img src="https://i.ibb.co/RzFPJz5/card-logo.png" alt="logo"></img>
             <h1>The Daily Pull</h1>
             <ul>
                 <li>shit</li>
